89ca25329551eb3e782a8790724cbb0b6025787f
Ernazar dev <maksetovernazar828@gmail.com>
2024-03-18 16:47:10 +0500

    Admin, Category Seeders are completed

--- database/migrations/2023_02_12_063708_create_categories_table.php
+++ database/migrations/2023_02_12_063708_create_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignIdFor(Img::class)->constrained('imgs');
+            $table->string('img_name');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 
+use App\Models\Category;
+use App\Models\Region;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,9 +19,73 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
+        // AdminFactory::factory(1)->create();
+        // SuperAdminFactory::factory(1)->create();
+
+        // \App\Models\User::create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-    }
+            Region::create([
+                'name' => [
+                    "kar" => "Nokis qalasi",
+                    "en" => "Nukus City",
+                    "uz" => "Nukus shahri",
+                    "ru" => "Город Нукус"
+                ]
+            ]);
+
+            User::create([
+            'username' => 'superadmin',
+            'phone' => '123',
+            'region_id' => 1,
+            'password' => Hash::make('superadminpassword'),
+            'role' => 'superadmin',
+            ]);
+
+            User::create([
+            'username' => 'admin',
+            'phone' => '123',
+            'region_id' => 1,
+            'password' => Hash::make('adminpassword'),
+            'role' => 'admin',
+            ]);
+
+            Category::create([
+                'name' => 'At', 
+                'img_name' => 'at.png'
+            ]);
+            
+            Category::create([
+                'name' => 'Eshki', 
+                'img_name' => 'eshki.png'
+            ]);
+            
+            Category::create([
+                'name' => 'Iyt', 
+                'img_name' => 'iyt.png'
+            ]);
+            
+            Category::create([
+                'name' => 'Mal', 
+                'img_name' => 'mal.png'
+            ]);
+
+            Category::create([
+                'name' => 'Tawiq', 
+                'img_name' => 'tawiq.png'
+            ]);
+            
+            Category::create([
+                'name' => 'Toti', 
+                'img_name' => 'toti.png'
+            ]);
+
+            Category::create([
+                'name' => 'Tuyetawiq', 
+                'img_name' => 'tuyetawiq.png'
+            ]);
+            
+            
+        }
 }
