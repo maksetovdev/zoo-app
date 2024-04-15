1c40fd6802423d79726a7bf9bc43749e8dc7294a
Ernazar dev <maksetovernazar828@gmail.com>
2024-04-15 21:37:45 +0500

    middleware completed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\SuperAdminMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use App\Http\Middleware\AdminMiddleware;
 
 class Kernel extends HttpKernel
 {
@@ -64,5 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'superadmin' => SuperAdminMiddleware::class,
+        'admin' => AdminMiddleware::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -8,17 +8,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AllAnimal;
 use App\Http\Controllers\AdminController;
 
-Route::apiResource('admins',AdminController::class);
+//superadmin
+Route::middleware(['auth:sanctum','superadmin'])->apiResource('admins',AdminController::class);
+//admin
+Route::middleware(['auth:sanctum','admin'])->apiResource('region',RegionController::class);
+//all users
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::apiResource('user',UserController::class)->except('index','show');
 Route::post('user/login',[UserController::class,'login']);
 Route::get('user/profile',[UserController::class,'show'])->middleware('auth:sanctum');
-
-Route::apiResource('region',RegionController::class);
-
 Route::middleware('auth:sanctum')->apiResource('animal', AnimalController::class);
-
 Route::middleware('auth:sanctum')->apiResource('animals', AllAnimal::class)->except('destroy','store');
