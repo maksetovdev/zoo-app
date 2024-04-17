149efd36088740bd52725b9bef2b874085460d34
Ernazar dev <maksetovernazar828@gmail.com>
2024-04-17 16:50:58 +0500

    code review beautiful 404 exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -22,8 +22,10 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->renderable(function (NotFoundHttpException $e) {
-            return response()->json(['message' => 'Object not found'], 404);
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if($request->wantsJson()) {
+                return response()->json(['message' => 'Object not found'], 404);
+            }
         });
     }
 }
